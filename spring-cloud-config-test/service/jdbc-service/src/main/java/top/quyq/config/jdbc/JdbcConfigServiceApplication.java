5389747f4bfba007d07d15877f1af0136fca0ac1
Qu-Yongquan <quyongquan@qq.com>
2020-03-01 20:52:04 +0800

    service 服务端测试

---
+++ spring-cloud-config-test/service/jdbc-service/src/main/java/top/quyq/config/jdbc/JdbcConfigServiceApplication.java
@@ -0,0 +1,15 @@
+package top.quyq.config.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class JdbcConfigServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcConfigServiceApplication.class,args);
+    }
+
+}
